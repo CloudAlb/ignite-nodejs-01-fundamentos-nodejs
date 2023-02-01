7bbf0ba5261d0ed9ce8cb64ad4c609d1b8430a0d
Matheus Albino <matheusalb3213@gmail.com>
2023-02-01 17:24:01 -0300

    Change formatting of .js files

--- streams/buffers.js
+++ streams/buffers.js
@@ -1,4 +1,4 @@
 // guarda informações de forma binária na memória com dígitos hexadecimais
 // é mais rápido que guardar strings, que são bem mais pesadas
-const buf = Buffer.from("okk")
-console.log(buf)
+const buf = Buffer.from("okk");
+console.log(buf);
--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -1,48 +1,49 @@
-import { Readable, Transform, Writable } from 'node:stream'
+import { Readable, Transform, Writable } from "node:stream";
 
 // reescrita de uma Stream (de leitura - readable)
 // só consigo ler os dados dela
 class OneToHundredStream extends Readable {
-    index = 1
+  index = 1;
 
-    _read() { // método obrigatório de toda stream de leitura
-        const i = this.index++
+  _read() {
+    // método obrigatório de toda stream de leitura
+    const i = this.index++;
 
-        setTimeout(() => {
-            if (i > 100) {
-                this.push(null)
-            } else {
-                const buf = Buffer.from(String(i))
+    setTimeout(() => {
+      if (i > 100) {
+        this.push(null);
+      } else {
+        const buf = Buffer.from(String(i));
 
-                this.push(buf) // esse método aceita Buffer, não tipos primitivos
-            }
-        }, 1000)
-    }
+        this.push(buf); // esse método aceita Buffer, não tipos primitivos
+      }
+    }, 1000);
+  }
 }
 
 // reescrita de uma stream (de transformação - transform)
 // serve como uma stream intermediária
 // obrigatoriamente lê dados de algum lugar e escreve os dados em outro lugar
 class InverseNumberStream extends Transform {
-    _transform(chunk, encoding, callback) {
-        const transformed = Number(chunk.toString()) * - 1
+  _transform(chunk, encoding, callback) {
+    const transformed = Number(chunk.toString()) * -1;
 
-        callback(null, Buffer.from(String(transformed)))
-    }
+    callback(null, Buffer.from(String(transformed)));
+  }
 }
 
 // reescrita de uma stream (de escrita - writable)
 // só consigo escrever os dados para ela
 class MultiplyByTenStream extends Writable {
-    // streams nunca retornam algo
+  // streams nunca retornam algo
 
-    // método obrigatório de streams de escrita
-    _write(chunk, encoding, callback) {
-        console.log(Number(chunk.toString()) * 10)
-        callback()
-    }
+  // método obrigatório de streams de escrita
+  _write(chunk, encoding, callback) {
+    console.log(Number(chunk.toString()) * 10);
+    callback();
+  }
 }
 
 new OneToHundredStream()
-    .pipe(new InverseNumberStream())
-    .pipe(new MultiplyByTenStream()) // cria uma stream e a transfere a stream de MultiplyByTen
+  .pipe(new InverseNumberStream())
+  .pipe(new MultiplyByTenStream()); // cria uma stream e a transfere a stream de MultiplyByTen
